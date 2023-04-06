e4818714effc08d12211eebebf9fa7dae3ea91ef
t-mario-y <t.mario.yoshi@gmail.com>
2023-04-07 03:49:54 +0900

    add layer

--- boards/t-mario-y_11111/main.py
+++ boards/t-mario-y_11111/main.py
@@ -1,19 +1,31 @@
 from kb import KMKKeyboard
 from kmk.keys import KC
+from kmk.modules.layers import Layers
 from kmk.modules.split import Split, SplitType
 
 keyboard = KMKKeyboard()
 
+layers_ext = Layers()
 split = Split(
     split_type=SplitType.UART, split_flip=True, data_pin=keyboard.data_pin, use_pio=True
 )
-keyboard.modules.append(split)
+keyboard.modules = [layers_ext, split]
 
+LOWER = KC.MO(1)
+RAISE = KC.MO(2)
 # fmt: off
 keyboard.keymap = [
     [
-        KC.Q,  KC.W,  KC.E,  KC.R,
-        KC.A,  KC.S,  KC.D,  KC.F
+        KC.Q,    KC.W,    KC.E,    KC.R,
+        KC.A,    LOWER,   RAISE,   KC.F,
+    ],
+    [
+        KC.N1,   KC.N2,   KC.N3,   KC.N4,
+        KC.N5,   LOWER,   RAISE,   KC.N6,
+    ],
+    [
+        KC.LEFT, KC.UP,   KC.DOWN, KC.RIGHT,
+        KC.LANG1,LOWER,   RAISE,   KC.LANG2,
     ]
 ]
 
